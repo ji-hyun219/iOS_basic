a9f72f08ae9a0c68154a96f6ae5a66e33421a1bd
ji-hyun219 <ix219@hanmail.net>
2023-05-05 17:45:54 +0900

    feat(UIButton): 아웃렛 변수, 액션 함수 추가

--- iOSBasic/ViewController.swift
+++ iOSBasic/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // 아웃렛 변수가 들어가는 곳
+    // Interface builder : Xcode에서 제공하는 GUI 기반의 앱 개발 도구
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // 액션 함수가 들어가는 곳
+    @IBAction func onBtn1Click(_ sender: UIButton) {
+        print("btn1 !!")
+    }
+    @IBAction func onBtn2Click(_ sender: UIButton) {
+        print("btn2 !!")
+    }
+    @IBAction func onBtn3Click(_ sender: UIButton) {
+        print("btn3 !!")
+    }
+    
+    
 }
 

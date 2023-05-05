600f246b1ff8a91bc3fac5bbd60e4806d272f201
ji-hyun219 <ix219@hanmail.net>
2023-05-05 16:31:24 +0900

    Initial Commit

---
+++ iOSBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBasic
+//
+//  Created by 이지현 on 2023/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

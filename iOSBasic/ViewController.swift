cb9af2b4ca2f20985483a0b54191b533e9426e93
ji-hyun219 <ix219@hanmail.net>
2023-05-05 18:25:08 +0900

    feat(UITextField, UIImageView): 입력하면 타이틀 수정, 이미지 교체

--- iOSBasic/ViewController.swift
+++ iOSBasic/ViewController.swift
@@ -12,15 +12,33 @@ class ViewController: UIViewController {
     
     // 아웃렛 변수가 들어가는 곳
     // Interface builder : Xcode에서 제공하는 GUI 기반의 앱 개발 도구
+    @IBOutlet weak var labelTitle: UILabel!
     @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var btn2: UIButton!
     @IBOutlet weak var btn3: UIButton!
     
     @IBOutlet var buttons: [UIButton]!
     
+    @IBOutlet weak var tfInput: UITextField!
+    
+    @IBOutlet weak var tfButton: UIButton!
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelTitle.text = "타이틀입니다"
+        
+        
+        for btn in buttons {
+            btn.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        }
+        
+        buttons[0].backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+        
+        
     }
 
     // 액션 함수가 들어가는 곳
@@ -28,12 +46,15 @@ class ViewController: UIViewController {
         print("btn1 !!")
     }
     @IBAction func onBtn2Click(_ sender: UIButton) {
-        print("btn2 !!")
+        image.image = UIImage(named: "image2.jpeg")
     }
     @IBAction func onBtn3Click(_ sender: UIButton) {
         print("btn3 !!")
     }
     
+    @IBAction func ontfButtonClick(_ sender: UIButton) {
+        labelTitle.text = tfInput.text
+    }
     
 }
 

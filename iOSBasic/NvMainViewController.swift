452075a34b62db1d77ea29096c981bc37ba1024e
ji-hyun219 <ix219@hanmail.net>
2023-05-13 16:45:10 +0900

    fix(NavigationController): 뷰 관리 방법

--- iOSBasic/NvMainViewController.swift
+++ iOSBasic/NvMainViewController.swift
@@ -14,6 +14,21 @@
 // 수직적인 이동은 간단히 떴다가 사라지는 화면으로 이해하면 좋다
 
 
+// < 네비게이션 컨트롤러 뷰 관리 방법 >
+// 다음 화면으로 이동하기
+//  self.navigationController?.pushViewController
+// 이전 화면으로 이동하기
+//  popViewController
+// 맨 앞으로 이동하기
+//  popToRootViewController
+// 특정번째 뷰로 이동하기 (2번째 3번째)
+//  popToViewController(newVC, animated: true)
+// 특정번째 뷰를 삭제할 때
+//  navigationController?.viewControllers 배열에서 삭제, remove(at:)
+// 특정뷰를 중간에 추가할 때
+//  navigationController?.viewControllers 배열에서 추가. insert(at:)
+
+
 import UIKit
 
 class NvMainViewController: UIViewController {

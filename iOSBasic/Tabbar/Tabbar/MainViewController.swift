c9b2685475ddc495972ffd825900eeeb72698cf3
ji-hyun219 <ix219@hanmail.net>
2023-05-21 10:30:11 +0900

    feat(Tabbar): New Project 생성

---
+++ iOSBasic/Tabbar/Tabbar/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tabbar
+//
+//  Created by 이지현 on 2023/05/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

22ca8da531a05527e84bbbec5aa8f133f1613622
ji-hyun219 <ix219@hanmail.net>
2023-05-21 11:04:19 +0900

    fix(Tabbar): tabBarController, navigationController 혼용

--- iOSBasic/Tabbar/Tabbar/MainViewController.swift
+++ iOSBasic/Tabbar/Tabbar/MainViewController.swift
@@ -11,9 +11,25 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // print("tabIndex: ", self.tabBarController?.selectedIndex)
+        // 위 출력 시, tabIndex 가 이상한 값이 나오는 이유
+        // >> 뷰가 정렬되고 있는데 시간이 걸리기 때문
+        // >> viewDidAppear 에다가 출력!
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let index = self.tabBarController?.selectedIndex {
+            print("tabIndex:", index)
+        } else {
+            print("nil")
+        }
     }
 
-
+    
+    @IBAction func onBtnNext(_ sender: UIButton) {
+        self.tabBarController?.selectedIndex = 1
+    }
+    
 }
 

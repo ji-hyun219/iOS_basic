ee60c24d009a80174f3fc52e7da1cd8f20fadeca
ji-hyun219 <ix219@hanmail.net>
2023-09-10 16:47:19 +0900

    feat(SideMenu): Project Init

---
+++ iOSBasic/sideMenu/sideMenu/MySideMenuNavigationController.swift
@@ -0,0 +1,20 @@
+//
+//  MySideMenuNavigationController.swift
+//  sideMenu
+//
+//  Created by 이지현 on 2023/09/10.
+//
+
+import UIKit
+import SideMenu
+
+class MySideMenuNavigationController: SideMenuNavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
